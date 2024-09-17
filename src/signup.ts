76d93a40c610d612ccb48dd745bb17a41899c6d7
Heitor Belém <heitorlbelem98@gmail.com>
2024-09-17 15:13:03 -0300

    Separa lógica dos Resources em outros métodos
    Eles não pertencem ao arquivo atual pois não participam da regra de negócio. São da camada de persistência
    
    Vamos remover posteriormente

--- src/signup.ts
+++ src/signup.ts
@@ -2,26 +2,44 @@ import crypto from "crypto"
 import pgp from "pg-promise"
 import { validateCpf } from "./validateCpf"
 
-export async function signup(input: any) { 
+async function getAccountByEmail(email: string) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app")
-	try {
-		const id = crypto.randomUUID();
-		const [accountAlreadyExist] = await connection.query("select * from ccca.account where email = $1", [input.email])
-		if (accountAlreadyExist) throw new Error("Account already exist")
-		if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid name")
-		if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid e-mail")
-		if (!validateCpf(input.cpf)) throw new Error("Invalid CPF")
-		if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid car plate")
-		await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password])
-		return { accountId: id }
-	} finally {
-		await connection.$pool.end()
-	}
+	const [accountData] = await connection.query("select * from ccca.account where email = $1", [email])
+	await connection.$pool.end()
+	return accountData
 }
 
-export async function getAccount(id: string) {
+async function getAccountById(id: string) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app")
 	const [accountData] = await connection.query("select * from ccca.account where account_id = $1", [id])
 	await connection.$pool.end()
+	return accountData
+}
+
+async function createAccount(account: any) {
+	const { name, email, password, cpf, isDriver, isPassenger, carPlate } = account
+	const id = crypto.randomUUID()
+	const connection = pgp()("postgres://postgres:123456@localhost:5432/app")
+	await connection.query(
+		"insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)",
+		[id, name, email, cpf, carPlate, !!isPassenger, !!isDriver, password]
+	)
+	await connection.$pool.end()
+	return { id }
+}
+
+export async function signup(input: any) { 
+	const accountAlreadyExist = await getAccountByEmail(input.email)
+	if (accountAlreadyExist) throw new Error("Account already exist")
+	if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid name")
+	if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid e-mail")
+	if (!validateCpf(input.cpf)) throw new Error("Invalid CPF")
+	if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid car plate")
+	const { id } = await createAccount(input)
+	return { accountId: id }
+}
+
+export async function getAccount(id: string) {
+	const accountData = await getAccountById(id)
 	return accountData
 }
