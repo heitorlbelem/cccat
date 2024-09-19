fe72dab4763bc91a64eb4886dd76abc94bf103cb
Heitor Belém <heitorlbelem98@gmail.com>
2024-09-19 17:11:58 -0300

    Adiciona sinon ao projeto e cria testes usando stub, spy e mock

--- test/signup.test.ts
+++ test/signup.test.ts
@@ -1,5 +1,6 @@
 import { AccountDAOInMemory } from "../src/AccountDAO"
 import { Signup } from "../src/Signup"
+import sinon from "sinon"
 
 let signup: Signup
 
@@ -75,4 +76,53 @@ test("Não deve criar um motorista com placa inválida", async () => {
         carPlate: "123AAA",
     }
     await expect(() => signup.execute(input)).rejects.toThrow(new Error("Invalid car plate"))
+})
+
+test("Deve criar um passageiro com stub", async () => {
+    const getAccountByEmail = sinon.stub(
+        AccountDAOInMemory.prototype, "getAccountByEmail"
+    ).resolves()
+    const input = {
+        name: "John Doe",
+        cpf: "01234567890",
+        email: `john.doe${Math.random()}@sample.com`,
+        password: "123456",
+        isPassenger: true
+    }
+    const signupOutput = await signup.execute(input)
+    expect(signupOutput.accountId).toBeDefined()
+    getAccountByEmail.restore()
+})
+
+test("Deve criar um passageiro com spy", async () => {
+    const accountDAOSpy = sinon.spy(AccountDAOInMemory.prototype, "getAccountByEmail")
+    const input = {
+        name: "John Doe",
+        cpf: "01234567890",
+        email: `john.doe${Math.random()}@sample.com`,
+        password: "123456",
+        isPassenger: true
+    }
+    const signupOutput = await signup.execute(input)
+    expect(signupOutput.accountId).toBeDefined()
+    expect(accountDAOSpy.calledWith(input.email)).toBe(true)
+    accountDAOSpy.restore()
+})
+
+test.only("Deve criar um passageiro com mock", async () => {
+    const accountDAOMock = sinon.mock(AccountDAOInMemory.prototype)
+    const input = {
+        name: "John Doe",
+        cpf: "01234567890",
+        email: `john.doe${Math.random()}@sample.com`,
+        password: "123456",
+        isPassenger: true
+    }
+    accountDAOMock.expects("getAccountByEmail").withArgs("john.doe@sample.com").once().callsFake(() => {
+        console.log("oi")
+    })
+    const signupOutput = await signup.execute(input)
+    expect(signupOutput.accountId).toBeDefined()
+    accountDAOMock.verify()
+    accountDAOMock.restore()
 })
