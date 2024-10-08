5b1f55d7d400678dbf964a9e566f000e1c6f7019
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-08 01:53:26 -0300

    Remove exemplos de test patterns

--- backend/ride/test/signup.test.ts
+++ backend/ride/test/signup.test.ts
@@ -3,7 +3,6 @@ import { Registry } from "../src/DI";
 import GetAccount from "../src/GetAccount";
 import { MailerGatewayMemory } from "../src/MailerGateway";
 import Signup from "../src/Signup";
-import sinon from "sinon";
 
 let signup: Signup;
 let getAccount: GetAccount;
@@ -55,69 +54,4 @@ test("Não deve criar a conta de um passageiro duplicado", async function () {
 	};
 	await signup.execute(input);
 	await expect(() => signup.execute(input)).rejects.toThrow(new Error("Duplicated account"));
-});
-
-test("Deve criar a conta de um passageiro com stub", async function () {
-	const mailerStub = sinon.stub(MailerGatewayMemory.prototype, "send").resolves();
-	const getAccountByEmail = sinon.stub(AccountRepositoryDatabase.prototype, "getAccountByEmail").resolves();
-	const input = {
-		name: "John Doe",
-		email: `john.doe@gmail.com`,
-		cpf: "97456321558",
-		password: "123456",
-		isPassenger: true
-	};
-	const outputSignup = await signup.execute(input);
-	expect(outputSignup.accountId).toBeDefined();
-	const outputGetAccount = await getAccount.execute(outputSignup.accountId);
-	expect(outputGetAccount.name).toBe(input.name);
-	expect(outputGetAccount.email).toBe(input.email);
-	expect(outputGetAccount.cpf).toBe(input.cpf);
-	expect(outputGetAccount.password).toBe(input.password);
-	mailerStub.restore();
-	getAccountByEmail.restore();
-});
-
-test("Deve criar a conta de um passageiro com spy", async function () {
-	const mailerSpy = sinon.spy(MailerGatewayMemory.prototype, "send");
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "97456321558",
-		password: "123456",
-		isPassenger: true
-	};
-	const outputSignup = await signup.execute(input);
-	expect(outputSignup.accountId).toBeDefined();
-	const outputGetAccount = await getAccount.execute(outputSignup.accountId);
-	expect(outputGetAccount.name).toBe(input.name);
-	expect(outputGetAccount.email).toBe(input.email);
-	expect(outputGetAccount.cpf).toBe(input.cpf);
-	expect(outputGetAccount.password).toBe(input.password);
-	expect(mailerSpy.calledOnce).toBe(true);
-	expect(mailerSpy.calledWith(input.email, "Welcome!", "...")).toBe(true);
-	mailerSpy.restore();
-});
-
-test("Deve criar a conta de um passageiro com mock", async function () {
-	const mailerMock = sinon.mock(MailerGatewayMemory.prototype);
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "97456321558",
-		password: "123456",
-		isPassenger: true
-	};
-	mailerMock.expects("send").withArgs(input.email, "Welcome!", "...").once().callsFake(() => {
-		console.log("abc");
-	});
-	const outputSignup = await signup.execute(input);
-	expect(outputSignup.accountId).toBeDefined();
-	const outputGetAccount = await getAccount.execute(outputSignup.accountId);
-	expect(outputGetAccount.name).toBe(input.name);
-	expect(outputGetAccount.email).toBe(input.email);
-	expect(outputGetAccount.cpf).toBe(input.cpf);
-	expect(outputGetAccount.password).toBe(input.password);
-	mailerMock.verify();
-	mailerMock.restore();
 });
